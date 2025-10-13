52477c4bb653f3b1fe48243bba89cd97c3bf1478
Dhaniutsman <saurusdan74@gmail.com>
2025-10-13 19:05:21 +0700

    latihan_aja_
    Ini hanya untuk latihan saya

---
+++ C++/latihan_aja.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int rup1;
+    int rup2;
+
+    cout<<"Enter your first rupiah sir :";
+    cin>>rup1;
+    cout<<endl;
+
+    // Second Rupiah's
+
+    cout<<"Enter your second rupiah sir :";
+    cin>>rup2;
+    cout<<endl;
+    return 0;
+
+    //Plus 
+
+    cout<<"Your current rupiah is :";
+    cout<<rup1 + rup2<<endl;
+    return 0;
+    
+
+
+
+}
