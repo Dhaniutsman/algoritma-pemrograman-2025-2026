147734de037ce2d1994cc00233c9a5c433d348e2
Dhaniutsman <saurusdan74@gmail.com>
2025-10-14 01:03:08 +0700

    commit4

---
+++ JAVA/coba1.java
@@ -0,0 +1,14 @@
+class coba1{
+    public static void main(String[] args) {
+        String nim = "I.2510435";
+        String nama = "Utsman Dhani P";
+        String umur = "19";
+        String profesi = "Mahasiswa";
+
+        System.out.println(nim); 
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(profesi);
+
+    }
+}
